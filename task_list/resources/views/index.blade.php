6941eb7361ea0824c0f3f6d4275f440ff73a4939
Zarko Stanisic <zarko.stanisic@live.com>
2026-03-06 10:48:28 +0100

    =6

---
+++ task_list/resources/views/index.blade.php
@@ -0,0 +1,5 @@
+Hello I'm a blade template!
+
+@isset($name)
+The name is: {{ $name }}
+@endisset
--- task_list/routes/web.php
+++ task_list/routes/web.php
@@ -3,25 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/test', function (){
-    return 'Test';
-});
-
-Route::get('/xxx', function () {
-    return 'Hello';
-})->name('hello');
-
-Route::get('/greet/{name}', function ($name) {
-    return 'Hello, ' . $name;
-});
-
-Route::get('/hallo', function () {
-    return redirect()->route('hello');
-});
-
-Route::fallback(function () {
-    return "Still got somewhere!";
+    return view('index', [
+        'name' => 'Zarko'
+    ]);
 });
