c0e320e3dc1fe1ea227cb60a0ef24f35466fc1f2
Zarko Stanisic <zarko.stanisic@live.com>
2026-03-09 09:14:13 +0100

    =12

--- task_list/database/seeders/DatabaseSeeder.php
+++ task_list/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\task;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,11 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Users
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        User::factory(10)->unverified()->create();
+
+        // Tasks
+        Task::factory(50)->create();
     }
 }
