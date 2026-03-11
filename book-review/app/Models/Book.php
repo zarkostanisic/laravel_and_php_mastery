14ef22daad251261457c1a25f731ed5c9a214b9e
Zarko Stanisic <zarko.stanisic@live.com>
2026-03-11 15:19:35 +0100

    =4

---
+++ book-review/app/Models/Book.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    public function reviews(){
+        return $this->hasMany(Review::class);     
+    }
+}
