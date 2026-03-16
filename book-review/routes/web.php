0ea7b7d1394147e512106c1747fbe69ffdeb5bec
Zarko Stanisic <zarko.stanisic@live.com>
2026-03-16 09:38:23 +0100

    =10

--- book-review/routes/web.php
+++ book-review/routes/web.php
@@ -3,9 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-   $books = App\Models\Book::with('reviews')->get();
+   $title = "ss";
+   $books = App\Models\Book::with('reviews')
+      ->withCount('reviews')
+      ->popular()
+      ->highestRated()
+      ->title($title)
+      // ->limit(3)
+      // ->withAvg('reviews', 'rating')
+      // ->orderBy('reviews_avg_rating', 'desc')
+      // ->having('reviews_count', '>=', 6)
+      ->get();
 
    foreach($books as $book){
-    var_dump($book->title);
+    var_dump($book->title, $book->reviews_count, $book->reviews_avg_rating, '<br/>');
    }
 });
