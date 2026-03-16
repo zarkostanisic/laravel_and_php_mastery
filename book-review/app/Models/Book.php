d60dd3d8901b0e4d38a9712b0432cd1d56451f3f
Zarko Stanisic <zarko.stanisic@live.com>
2026-03-16 10:34:22 +0100

    =12

--- book-review/app/Models/Book.php
+++ book-review/app/Models/Book.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Book extends Model
 {
@@ -18,13 +19,31 @@ class Book extends Model
         return $query->where('title', 'like', '%' . $title . '%');
     }
 
-    public function scopePopular(Builder $query): Builder{
-        return $query->withCount('reviews')
-            ->orderBy('reviews_count', 'desc');
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder|QueryBuilder{
+        return $query->withCount([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ])
+        ->orderBy('reviews_count', 'desc');
     }
 
-    public function scopeHighestRated(Builder $query): Builder{
-        return $query->withAvg('reviews', 'rating')
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder|QueryBuilder{
+        return $query->withAvg([
+                'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+            ], 'rating')
         ->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder|QueryBuilder{
+        return $query->having('reviews_count', '>=', $minReviews);
+    }
+
+    private function dateRangeFilter(Builder $query, $from = null, $to = null){
+        if($from && !$to){
+            $query->where('created_at', '>=', $from);
+        } elseif(!$from && $to){
+            $query->where('created_at', '<=', $to);
+        } else{
+            $query->whereBetween('created_at', [$from, $to]);
+        }
     }
 }
--- book-review/routes/web.php
+++ book-review/routes/web.php
@@ -1,21 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
-Route::get('/', function () {
-   $title = "ss";
-   $books = App\Models\Book::with('reviews')
-      ->withCount('reviews')
-      ->popular()
-      ->highestRated()
-      ->title($title)
-      // ->limit(3)
-      // ->withAvg('reviews', 'rating')
-      // ->orderBy('reviews_avg_rating', 'desc')
-      // ->having('reviews_count', '>=', 6)
-      ->get();
-
-   foreach($books as $book){
-    var_dump($book->title, $book->reviews_count, $book->reviews_avg_rating, '<br/>');
-   }
-});
+Route::redirect('/', 'books');
+Route::resource('books', BookController::class);
