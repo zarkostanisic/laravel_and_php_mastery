395c1bc03b1ab415ea50e79c016722a520a8602c
Zarko Stanisic <zarko.stanisic@live.com>
2026-03-18 12:11:16 +0100

    =4

---
+++ events_management/app/Models/Attendee.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Attendee extends Model
+{
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+    public function event(): BelongsTo {
+        return $this->belongsTo(Event::class);
+    }
+}
--- events_management/routes/api.php
+++ events_management/routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\EventController;
+use App\Http\Controllers\Api\AttendeeController;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('events', EventController::class);
+Route::apiResource('events.attendees', AttendeeController::class)
+    ->scoped(['attendee' => 'event']);
