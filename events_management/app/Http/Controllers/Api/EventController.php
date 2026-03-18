f898675a60296b260f0ccf51c49e49461f1f7b38
Zarko Stanisic <zarko.stanisic@live.com>
2026-03-18 15:22:11 +0100

    =8

--- events_management/app/Http/Controllers/Api/EventController.php
+++ events_management/app/Http/Controllers/Api/EventController.php
@@ -14,8 +14,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        return Event::with(['user', 'attendees', 'attendees.user', 'attendees.event'])
-            ->get();
+        return Event::all();
     }
 
     /**
@@ -23,7 +22,19 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'start_time' => 'required|date',
+            'end_time' => 'required|date|after:start_time'
+        ]);
+
+        $event = Event::create([
+            ...$data,
+            'user_id' => 1
+        ]);
+
+        return $event;
     }
 
     /**
@@ -37,16 +48,29 @@ class EventController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
-    {
-        //
+    public function update(Request $request, Event $event)
+    {   
+        $data = $request->validate([
+            'name' => 'sometimes|string|max:255',
+            'description' => 'nullable|string',
+            'start_time' => 'sometimes|date',
+            'end_time' => 'sometimes|date|after:start_time'
+        ]);
+
+        $event->update([
+            ...$data
+        ]);
+
+        return $event;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Event $event)
     {
-        //
+        $event->delete();
+
+        return response(status: 204);
     }
 }
